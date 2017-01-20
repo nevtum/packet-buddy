3fce254de3cc734086063df9f3545b079bc6b2f8
Neville Tummon <nt.devs@gmail.com>
2017-01-20 18:57:12 +1100

    preparing client side javascript imports

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ path = require('path');
 
 app = express();
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, './static/index.html'))
 });
 
