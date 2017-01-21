c394d1472e4031d731b8a887f92039a5a3d6b437
Neville Tummon <nt.devs@gmail.com>
2017-01-22 02:36:02 +1100

    implemented proper crc calculation

--- src/xseries/utilities.js
+++ src/xseries/utilities.js
@@ -1,8 +1,13 @@
 
 var ChecksumCalculator = {
     calculateCRC16(array) {
-        // obviously not correct
-        return [6, 7];
+        var mod = 0x00;
+
+        array.forEach(function(element) {
+            mod = mod ^ element;
+        }, this);
+        
+        return [0x0F & mod, 0xF0 & mod];
     }
 }
 
