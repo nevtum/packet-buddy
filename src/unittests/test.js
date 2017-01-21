c53d62809727d91fb74b8ca06998684fa46de78e
Neville Tummon <nt.devs@gmail.com>
2017-01-22 03:01:03 +1100

    passing critical path unit test

--- src/unittests/test.js
+++ src/unittests/test.js
@@ -4,14 +4,14 @@ var calc = require('../xseries/utilities');
 describe('Checksum calculator', function() {
   describe('#calculateCRC16()', function() {
     describe('when a valid array is passed in', function() {
-      var result = calc.calculateCRC16([1, 2, 3, 4]);
+      var result = calc.calculateCRC16([72, 34, 28, 4]);
       
       it('should return an array of length 2', function() {
         assert.equal(result.length, 2);
       });
 
       it('should return correct CRC', function() {
-        assert.deepEqual(result, [6, 7]);      
+        assert.deepEqual(result, [2, 112]);      
       });
     });
 
