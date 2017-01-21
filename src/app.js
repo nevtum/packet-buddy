111b309c2086159201ad5cc1bd455fd79b3238cf
Neville Tummon <nt.devs@gmail.com>
2017-01-21 14:21:14 +1100

    properly referencing static directory

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 express = require('express');
 path = require('path');
 
-app = express();
-
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, 'build');
+var publicPath = path.resolve(__dirname, '../build');
 
-app.use(express.static(publicPath));
+app = express();
+
+app.use('/public', express.static(publicPath));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './index.html'))
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.post('/api/parse', function(req, res) {
