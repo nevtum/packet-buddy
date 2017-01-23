915c51eec5c1dbf111f6dac75c730711e463fdc7
Neville Tummon <nt.devs@gmail.com>
2017-01-23 22:43:14 +1100

    bit neater styling to each decoded packet

--- src/Components/Packet.js
+++ src/Components/Packet.js
@@ -10,9 +10,46 @@ const Packet = function(props) {
         </div>
         );
     }
+
+    let classname = data.validCRC == true ? 
+        "packet" : "packet invalid";
+
+    let configTabulated = Object.keys(data.configData).map(function(key, index) {
+        return data.configData[key];
+    });
+
     return (
-        <div className="packet">
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div className={classname}>
+            <div className="summary">
+                <h4>{data.type}</h4>
+            </div>
+            <hr />
+            <div className="config">
+                <h5>Configuration data</h5>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Byte Range</th>
+                            <th>Hex</th>
+                            <th>Interpretation</th>
+                        </tr>
+                        {configTabulated.map(function(element, index) {
+                            return (
+                                <tr key={index}>
+                                    <td>{element.byteRange}</td>
+                                    <td>{element.hex}</td>
+                                    <td>{element.value}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <hr />            
+            <div className="config">
+                <h5>Meters</h5>
+                <pre>{JSON.stringify(data.meterData, null, 2)}</pre>
+            </div>
         </div>
     );
 }
