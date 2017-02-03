d8e9add903ed0d10c06541d2f7a0edbf67b8244c
Neville Tummon <nt.devs@gmail.com>
2017-02-03 22:59:27 +1100

    filter toggle complex enough to be own component

--- src/Components/ViewPageControls.js
+++ src/Components/ViewPageControls.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+const FilterToggleControl = (props) => {
+    let filterControls = null;
+    // To do. Bind a filter function through props passed
+    if (props.filterOptions.length > 1) {
+        let filterOptions = props.filterOptions.map(function(element) {
+            return (
+                <li key={element}>
+                    <a href="#">{element}</a>
+                </li>
+            );
+        });
+
+        filterControls = (
+            <div>
+                <p>Packets displayed</p>
+                <ul>
+                    {filterOptions}
+                </ul>
+            </div>
+        );
+    }
+
+    return filterControls;
+}
+
 const ViewControls = (props) => {
     let viewControls;
     if (props.decoded.length > 1) {
@@ -18,32 +43,10 @@ const ViewControls = (props) => {
         );
     }
 
-    let filterControls;
-    // To do. Bind a filter function through props passed
-    if (props.filterOptions.length > 1) {
-        let options = [...props.filterOptions, "All"];
-        let filterOptions = options.map(function(element) {
-            return (
-                <li key={element}>
-                    <a href="#">{element}</a>
-                </li>
-            );
-        });
-
-        filterControls = (
-            <div>
-                <p>Packets displayed</p>
-                <ul>
-                    {filterOptions}
-                </ul>
-            </div>
-        );
-    }
-
     return (
         <div id="controls">
             {viewControls}
-            {filterControls}
+            <FilterToggleControl filterOptions={props.filterOptions} />
         </div>
     );
 }
