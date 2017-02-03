41d5dbbd37f08e918653e5e33e7eba554e95d2c0
Neville Tummon <nt.devs@gmail.com>
2017-02-03 22:50:49 +1100

    some more refactoring to accomodate filter

--- src/Components/ViewPageControls.js
+++ src/Components/ViewPageControls.js
@@ -5,29 +5,45 @@ const ViewControls = (props) => {
     if (props.decoded.length > 1) {
         viewControls = (
             <div>
+                <a href="#" onClick={props.onSwitchEditMode}>Switch to edit mode</a>
                 | <a href="#" onClick={props.onAllCollapsed}>Collapse All</a>
                 | <a href="#" onClick={props.onAllExpanded}>Expand All</a>
             </div>);
     }
+    else {
+        viewControls = (
+            <div>
+                <a href="#" onClick={props.onSwitchEditMode}>Switch to edit mode</a>
+            </div>
+        );
+    }
 
     let filterControls;
     // To do. Bind a filter function through props passed
-    if (props.filterOptions.length > 0) {
-        filterControls = props.filterOptions.map(function(element) {
+    if (props.filterOptions.length > 1) {
+        let options = [...props.filterOptions, "All"];
+        let filterOptions = options.map(function(element) {
             return (
                 <li key={element}>
                     <a href="#">{element}</a>
-                </li>);
-        })
+                </li>
+            );
+        });
+
+        filterControls = (
+            <div>
+                <p>Packets displayed</p>
+                <ul>
+                    {filterOptions}
+                </ul>
+            </div>
+        );
     }
 
     return (
         <div id="controls">
-            <a href="#" onClick={props.onSwitchEditMode}>Switch to edit mode </a>
             {viewControls}
-            <ul>
-                {filterControls}
-            </ul>
+            {filterControls}
         </div>
     );
 }
